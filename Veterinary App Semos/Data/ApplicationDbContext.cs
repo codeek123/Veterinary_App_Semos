2596aa4157b1263b09d1a0bc9c76faaf90a1ee13
codeek123 <eva.koceva12@gmail.com>
2025-03-11 11:55:32 +0100

    Add project files.

---
+++ Veterinary App Semos/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Veterinary_App_Semos.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
