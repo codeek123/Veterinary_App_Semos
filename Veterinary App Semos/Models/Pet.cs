f7d0c3b9630237ec1b132b11c09566f0f1c234f0
codeek123 <eva.koceva12@gmail.com>
2025-03-11 12:03:52 +0100

    Add pet and vaccine models

---
+++ Veterinary App Semos/Models/Pet.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Veterinary_App_Semos.Models
+{
+    public class Pet
+    {
+        public int PetId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        [Range(0, 50, ErrorMessage = "Age must be between 0 and 50 years.")]
+        public int Age { get; set; }
+        public int OwnerId { get; set; }
+        public Owner Owner { get; set; }
+
+        public List<Vaccine> Vaccines { get; set; } = new List<Vaccine>();
+
+        public string GetDetailsForPet()
+        {
+            return $"Pet Name: {Name}, Age: {Age}, Owner ID: {OwnerId}";
+        }
+
+        [NotMapped]
+        public List<int> VaccinesNames { get; set; }
+    }
+}
---
+++ Veterinary App Semos/Models/Vaccine.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Veterinary_App_Semos.Models
+{
+    public class Vaccine
+    {
+        public int VaccineId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public List<Pet> Pets { get; set; } = new List<Pet>();
+    }
+}
